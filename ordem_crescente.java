f08274b21ebb95ecd5062249fc01a3ac9a82c0db
Igor Maurilio <igoor.maurilio@gmail.com>
2024-11-27 01:13:19 -0300

    Add files via upload

---
+++ ordem_crescente.java
@@ -0,0 +1,27 @@
+package aula10;
+
+import java.util.Scanner;
+
+public class ordem_crescente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    //Ordem crescente
+		
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("Informe o primeiro valor: ");
+		int a = scn.nextInt();
+		
+		System.out.println("Informe o segundo valor: ");
+		int b = scn.nextInt();
+		
+		System.out.println("Informe o terceiro valor: ");
+		int c = scn.nextInt();
+				
+		if (a <= b && b >= c && c>a);
+		
+		
+	}
+
+}
